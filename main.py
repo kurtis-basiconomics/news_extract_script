3d351a98f8798faabb71d98eb8a2d4c8becf7511
kurtis-basiconomics <167732469+kurtis-basiconomics@users.noreply.github.com>
2024-05-17 07:46:28 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,53 @@
+try:
+    import scrape_blmHdln
+    print('scrape Bloomberg complete')
+except Exception as e:
+    print('Bloomberg scrape with error:  ', e)
+
+
+try:
+    import scrape_wsjHdln
+    print('scrape WSJ complete')
+except Exception as e:
+    print('WSJ scrape with error:  ', e)
+
+
+try:
+    import scrape_mkwHdln
+    print('scrape Marketwatch complete')
+except Exception as e:
+    print('Marketwatch scrape with error:  ', e)
+
+
+try:
+    import scrape_cnb
+    print('scrape CNBC complete')
+except Exception as e:
+    print('CNBC scrape with error:  ', e)
+
+try:
+    import scrape_ft
+    print('scrape Bloomberg complete')
+except Exception as e:
+    print('Bloomberg scrape with error:  ', e)
+
+
+try:
+    import scrape_fp
+    print('scrape WSJ complete')
+except Exception as e:
+    print('WSJ scrape with error:  ', e)
+
+
+try:
+    import scrape_glb
+    print('scrape Marketwatch complete')
+except Exception as e:
+    print('Marketwatch scrape with error:  ', e)
+
+
+try:
+    import scrape_scm
+    print('scrape CNBC complete')
+except Exception as e:
+    print('CNBC scrape with error:  ', e)
